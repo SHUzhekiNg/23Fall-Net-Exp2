1b1fe99785ab85c6b1812198c79296c23c2fa889
SHUzhekiNg <zhenglicheng@yeah.net>
2023-09-13 09:13:18 +0800

    partly done

--- core/TCPServer.java
+++ core/TCPServer.java
@@ -12,8 +12,13 @@ class TCPLoginServer {
         String loginInfo = new String(Files.readAllBytes(Paths.get(filePath)));
         String[] lines = loginInfo.split("\n");
         String loginString;
+
+        FileWriter fileWriter = new FileWriter("login_info.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
         ServerSocket loginSocket = new ServerSocket(6789);
         boolean flag;
+
         while (true) {
             // login
             flag = false;
@@ -23,6 +28,8 @@ class TCPLoginServer {
             loginString = loginFromClient.readLine();
             for (String line : lines) {
                 if(loginString.equals(line)) {
+                    bufferedWriter.write(loginString);
+                    bufferedWriter.close();
                     loginMsgToClient.writeBytes("true\n");
                     loginConnectionSocket.close();
                     flag = true;
@@ -44,6 +51,7 @@ class TCPFileServer{
             
             byte[] buffer = new byte[1024];
             int bytesRead;
+            
             
             while ((bytesRead = fileInputStream.read(buffer)) != -1) {
                 // System.out.println("1");
--- frontend/GUIServer.java
+++ frontend/GUIServer.java
@@ -3,11 +3,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import core.ServerBooter;
+
 
 public class GUIServer extends JFrame{
     private JTextField usernameField;
     private JPasswordField passwordField;
-
+    
     public GUIServer() throws HeadlessException{
         this.setTitle("GUIServer");
         this.setSize(500, 250);
@@ -44,8 +52,9 @@ public class GUIServer extends JFrame{
 
                 // 这里可以添加验证逻辑，比如检查用户名和密码是否正确
                 // 这里只是一个示例，不进行实际验证
-                if (username.equals("admin") && password.equals("password")) {
-                    JOptionPane.showMessageDialog(GUIServer.this, "登录成功");
+                if (username.equals("admin") && password.equals("123")) {
+                    dispose();
+                    createMainPage();
                 } else {
                     JOptionPane.showMessageDialog(GUIServer.this, "登录失败，请检查用户名和密码");
                 }
@@ -53,7 +62,6 @@ public class GUIServer extends JFrame{
         });
 
         this.add(jPanel);
-
         this.setVisible(true);
     }
 
@@ -72,7 +80,66 @@ public class GUIServer extends JFrame{
         panel.add(component, constraints);
     }
 
-    public static void main(String[] args){
+    private static Object[][] parseData(List<String> lines) {
+        Object[][] data = new Object[lines.size()][2];
+        for (int i = 0; i < lines.size(); i++) {
+            String[] parts = lines.get(i).split(" ");
+            if (parts.length == 2) {
+                data[i][0] = parts[0];
+                data[i][1] = parts[1];
+            }
+        }
+        return data;
+    }
+
+    private static List<String> readFileData(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    private String[] columnNames = {"username", "password"};
+    private JFrame mainFrame = new JFrame("用户登录信息");
+    private void updateTableData() {
+        List<String> lines = readFileData("login_info.txt");
+        Object[][] data = parseData(lines);
+
+        JTable table = new JTable(data, columnNames);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        mainFrame.getContentPane().removeAll(); // 清除旧的内容
+        mainFrame.add(scrollPane, BorderLayout.CENTER);
+        mainFrame.revalidate(); // 重新布局和绘制
+    }
+
+    private void createMainPage() {
+        // 创建新的主页面
+        
+        mainFrame.setSize(800, 600);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        mainFrame.setLayout(new BorderLayout());
+
+        // 创建一个定时器，每隔一段时间触发更新操作
+        Timer timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateTableData();
+            }
+        });
+        timer.start(); // 启动定时
+        mainFrame.setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        ServerBooter.main(null);
         new GUIServer();
     }
 }
